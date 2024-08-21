cb20f4e029d5b8cb6f15d28b5eaed56f18405176
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-21 11:23:36 +0530

    es6module import

---
+++ calces6module.mjs
@@ -0,0 +1,11 @@
+const message = 'From ES6 module';
+
+function Multiply(x,y){
+  return x*y;
+}
+
+function Division(x,y){
+  return x/y;
+}
+
+export {Multiply,Division,message};
---
+++ useEs6module.mjs
@@ -0,0 +1,5 @@
+import { Multiply,Division,message } from "./calces6module.mjs";
+
+console.log(message);
+console.log('Division:', Division(10,2));
+console.log('Multiplication:', Multiply(10,3));
