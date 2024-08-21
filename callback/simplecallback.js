99e2ab49ba9de5ddf5bebfd95af24688d2736f98
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-21 12:33:22 +0530

    setTimeout and setInterval examples added

--- callback/simplecallback.js
+++ callback/simplecallback.js
@@ -12,4 +12,19 @@ const calculateNameLength = (name,cb) =>{
     const length = name.length;
     cb(length)
 }
-calculateNameLength("Ashutosh",(length)=>{console.log("The length is" + length)})
+// calculateNameLength("Ashutosh",(length)=>{console.log("The length is" + length)})
+
+//timers and intervals
+// console.log("before the setTimeout")
+// var secondsInMilliSeconds = 1000
+// setTimeout(()=>{console.log("a second has passed")},secondsInMilliSeconds)
+// console.log("after the setTimeout")
+
+var secondsInMilliSeconds = 1000
+let totalExecution = 0;
+console.log('before setInterval')
+setInterval(()=>{
+    totalExecution++;
+    console.log(`a second has passed this is the ${totalExecution} execution`)
+},secondsInMilliSeconds)
+console.log('after setInterval')
