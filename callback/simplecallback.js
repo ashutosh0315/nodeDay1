17b0f3d2d88b16d65b26e434bfe3d86ee6934ae7
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-21 12:52:28 +0530

    error first callbacks

--- callback/simplecallback.js
+++ callback/simplecallback.js
@@ -1,10 +1,10 @@
-function doSomething(cb){
-    console.log("Do something.....")
-    cb()
-}
-const nextStep = ()=>{
-    console.log("Callback called")
-}
+// function doSomething(cb){
+//     console.log("Do something.....")
+//     cb()
+// }
+// const nextStep = ()=>{
+//     console.log("Callback called")
+// }
 
 // doSomething(nextStep)
 
@@ -20,11 +20,32 @@ const calculateNameLength = (name,cb) =>{
 // setTimeout(()=>{console.log("a second has passed")},secondsInMilliSeconds)
 // console.log("after the setTimeout")
 
-var secondsInMilliSeconds = 1000
-let totalExecution = 0;
-console.log('before setInterval')
-setInterval(()=>{
-    totalExecution++;
-    console.log(`a second has passed this is the ${totalExecution} execution`)
-},secondsInMilliSeconds)
-console.log('after setInterval')
+// var secondsInMilliSeconds = 1000
+// let totalExecution = 0;
+// console.log('before setInterval')
+// setInterval(()=>{
+//     totalExecution++;
+//     console.log(`a second has passed this is the ${totalExecution} execution`)
+// },secondsInMilliSeconds)
+// console.log('after setInterval')
+
+//error first callbacks
+
+
+const doSomething = (cb)=>{
+    const error = new Error("Something went wrong");
+    cb(error,null)
+    
+}
+
+
+doSomething((error=false,result)=>{
+    if(error){
+        console.log("There was an error")
+        return;
+    }
+    console.log("Result:", result)
+    console.log("Everything went well")
+
+})
+
