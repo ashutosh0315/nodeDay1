a2d822f0ec60aaff12ebf825432a9cf56453887b
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-21 11:44:57 +0530

    fourth

--- useEs6module.mjs
+++ useEs6module.mjs
@@ -1,5 +1,7 @@
 import { Multiply,Division,message } from "./calces6module.mjs";
-
+import simple from './calculator.js'
 console.log(message);
 console.log('Division:', Division(10,2));
 console.log('Multiplication:', Multiply(10,3));
+
+console.log("Addition" +simple.add(11,21))
