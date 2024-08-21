b683abc09124c10fc0e100dcf8d0a5f2627a9cf7
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-21 11:30:10 +0530

    import es6 module dynamically

--- simple.js
+++ simple.js
@@ -6,3 +6,9 @@ let addResult = add(5,6)
 console.log("Add result" + addResult)
 let area = pi * 3*3
 console.log("Area" +area)
+//to import es6 module dynamically
+import ('./calces6module.mjs').then(
+    es =>{ console.log(es.default)
+        console.log(es.Multiply(10,3))
+    }
+)
